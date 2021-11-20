6546a4336449ca0db4d8d3ad7c581f205ee8a90a
tokiya.horikawa <ayikot4423@gmail.com>
2021-11-20 22:07:51 +0900

    conected test for hasura

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+// client side query client
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
