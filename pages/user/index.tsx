7439645770b64c711565a367be810563f36d4c96
tokiya.horikawa <ayikot4423@gmail.com>
2021-11-21 17:52:27 +0900

    feat: make layout

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,34 +1,26 @@
-import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 import { useUsersQuery } from "../../graphql/graphql";
 import { graphqlClient } from "../../graphql/client";
+import Layout from "../../components/Layout";
 
-const title = "User Index";
+const title = "ユーザー一覧ページ";
 const UserPage = () => {
   const { data: dataUsers } = useUsersQuery(graphqlClient);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={title} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>{title}</h1>
-        <ul>
-          {dataUsers &&
-            dataUsers.users.map((user) => <li key={user.id}>{user.name} </li>)}
-        </ul>
-        <div>
-          <Link href="/">
-            <a className={styles.link}>ホームに戻る</a>
-          </Link>
-        </div>
-      </main>
-    </div>
+    <Layout title={title}>
+      <h1 className={styles.title}>{title}</h1>
+      <ul>
+        {dataUsers &&
+          dataUsers.users.map((user) => <li key={user.id}>{user.name} </li>)}
+      </ul>
+      <div>
+        <Link href="/">
+          <a className={styles.link}>ホームに戻る</a>
+        </Link>
+      </div>
+    </Layout>
   );
 };
 
