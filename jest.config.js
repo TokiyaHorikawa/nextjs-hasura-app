fab97deaa7399ccdf166f3774b3a77b4b3481dbd
tokiya.horikawa <ayikot4423@gmail.com>
2022-01-09 11:42:50 +0900

    chore: Setting jest conf

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      useBabelrc: true,
+      tsConfigFile: './tsconfig.jest.json',
+    },
+  },
 };
